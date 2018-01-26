8b1678289db66181cdf399cef6ff924a84992617
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2018-01-26 15:39:47 +0100

    Resized OD_Sample size

--- tests/testthat/test_netDir.R
+++ tests/testthat/test_netDir.R
@@ -8,13 +8,13 @@ test_that("netDir works", {
   # Run simple net test
   test <- make.netDir(OD_Sample)
   expect_output(str(test), "List of 3")
-  expect_output(str(nrow(test$netDir)), "5158")
+  expect_output(str(nrow(test$netDir)), "2182")
 })
 
 test_that("make.netDir with disp", {
   # Run simple net test
   test <- make.netDir(OD_Sample, disp = TRUE)
-  expect_output(str(nrow(test$netDir_disp)), "98")
+  expect_output(str(nrow(test$netDir_disp)), "33")
   expect_length(test$gDir_disp, 10)
   expect_length(test$nodes, 13)
   expect_output(str(class(test$netDir_disp)), "data.frame")
@@ -25,7 +25,7 @@ test_that("make.netDir with disp", {
 test_that("make.netDir with cap", {
   # Run simple net test
   test <- make.netDir(OD_Sample, cap = TRUE)
-  expect_output(str(nrow(test$netDir_cap)), "501")
+  expect_output(str(nrow(test$netDir_cap)), "205")
   expect_length(test$gDir_cap, 10)
   expect_length(test$nodes, 13)
   expect_output(str(class(test$netDir_cap)), "data.frame")
