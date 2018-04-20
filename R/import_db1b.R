aa1911b4f0b3b61c980d83978b9aaff0f89dd010
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2018-04-20 14:51:55 +0200

    typos

--- R/import_db1b.R
+++ R/import_db1b.R
@@ -127,7 +127,7 @@ ODRaw <- function(x,y){
 globalVariables(c("ITIN_ID", "MKT_ID", "SEQ_NUM", "YEAR", "QUARTER", "TRIP_BREAK",
                   "OPERATING_CARRIER", "DISTANCE", "GATEWAY", "ROUNDTRIP", "ITIN_YIELD",
                   "ITIN_FARE", "BULKFARE", "DISTANCE_FULL", "UNIQUE_CARRIER",
-                  "UNIQUE_CARRIER_NAME", "MONTH", "AIRCRAFT_CONFIG", "import_db1b"))
+                  "UNIQUE_CARRIER_NAME", "MONTH", "AIRCRAFT_CONFIG", "import_db1b", "CLASS"))
 
 pos = 1
 envir = as.environment(pos)
