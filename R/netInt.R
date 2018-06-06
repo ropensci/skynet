50494461f48e89fe50ad1a88becfb0d7578b0060
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2018-06-06 13:04:51 +0200

    make.netInt deprecated

--- R/netInt.R
+++ R/netInt.R
@@ -11,42 +11,17 @@
 #' @examples
 #' \dontrun{
 #'
-#' netInt(skynet_example("T100_2011_int.csv"), OD_Sample, 1)
+#' make.netInt(skynet_example("T100_2011_int.csv"), OD_Sample, 1)
 #'
 #' }
 #'
 #' @export
 #'
 
-make.netInt <- function(x = NULL, m, Q = NULL){
+make.netInt <- function(x = NULL, m = NULL, Q = NULL){
 
-  if(is.null(Q))
-    stop("Please select desired Quarter")
-  if(is.null(x))
-    stop("Please select T-100 International Segment file")
-
-# International option
-  International <- fread(x, header = TRUE, sep = ",", stringsAsFactors = FALSE,
-      integer64 = "numeric")
-
-  International <- International %>%
-    filter(CLASS == "F" | CLASS == "L", PASSENGERS > 0, QUARTER == Q) %>%
-    select(ORIGIN, ORIGIN_CITY_MARKET_ID, DEST, DEST_CITY_MARKET_ID,
-           PASSENGERS, ORIGIN_CITY_NAME, DEST_CITY_NAME) %>%
-    rename(origin = ORIGIN, dest = DEST, passengers = PASSENGERS,
-           origin_city_mkt_id = ORIGIN_CITY_MARKET_ID,
-           origin_city = ORIGIN_CITY_NAME,
-           dest_city_mkt_id = DEST_CITY_MARKET_ID,
-           dest_city = DEST_CITY_NAME) %>%
-    mutate(itin_fare = NA, itin_yield = NA, fare_sd = NA,
-           passengers = as.integer(passengers)) %>%
-    select(origin, dest, passengers, fare_sd, itin_fare, itin_yield,
-           origin_city_mkt_id, origin_city, dest_city_mkt_id, dest_city)
-
-  m <- as.data.frame(m)
-  netMergedInt <- rbind(m, International)
-
-  return(netMergedInt)
+warning("netImport function is deprecated, please use `import_t100()`,
+        generate your desired network and rbind() with the db1b network of choice.")
 
 }
 
