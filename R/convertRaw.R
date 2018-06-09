7644deb0a7e1d17e0a80d55b0d1ff2658333f3fc
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2018-06-09 09:58:39 +0200

    Fixed convertraw to match other import functions

--- R/convertRaw.R
+++ R/convertRaw.R
@@ -7,8 +7,8 @@
 #' syntax: \emph{"Origin_and_Destination_Survey_DB1BCoupon_year_quarter.csv", "Origin_and_Destination_Survey_DB1BTicket_year_quarter.csv".}
 #' Note: We do recommend sparklyr to be used for larger sets of data.
 #'
-#' @param x First csv file to be processed
-#' @param y Second csv file to be processed
+#' @param x Coupon csv file to be processed
+#' @param y Ticket csv file to be processed
 #' @param path Path to save file to
 #'
 #' @examples
@@ -24,31 +24,11 @@
 #' @export
 #'
 
-convertRaw <- function(x,y,path = NULL){
+convertRaw <- function(c,t,path = NULL){
 
   if(missing(path)){
     stop("Please specify the path argument.")
   }
-
-  if(grepl("Ticket", deparse(substitute(x)), ignore.case = TRUE) == TRUE)
-    t <- x
-  if(grepl("Ticket", deparse(substitute(y)), ignore.case = TRUE) == TRUE)
-    t <- y
-  if(grepl("Coupon", deparse(substitute(x)), ignore.case = TRUE) == TRUE)
-    c <- x
-  if(grepl("Coupon", deparse(substitute(y)), ignore.case = TRUE) == TRUE)
-    c <- y
-
-
-  Ticket_temp <- fread(t, header = TRUE, sep = ",", stringsAsFactors = FALSE,
-                       integer64 = "numeric")
-
-  Ticket_temp <- Ticket_temp %>%
-    select("ItinID", "RoundTrip", "FarePerMile", "Passengers",
-           "ItinFare", "BulkFare", "Distance") %>%
-    rename(ITIN_ID = ItinID, ROUNDTRIP = RoundTrip,
-           ITIN_YIELD = FarePerMile, PASSENGERS = Passengers,
-           ITIN_FARE = ItinFare, BULKFARE = BulkFare, DISTANCE_FULL = Distance)
 
   Coupon_temp <- fread(c, header = TRUE, sep = ",", stringsAsFactors = FALSE,
                        integer64 = "numeric")
@@ -65,6 +45,16 @@ convertRaw <- function(x,y,path = NULL){
            DEST = Dest, TRIP_BREAK = Break,
            OPERATING_CARRIER = OpCarrier,
            DISTANCE = Distance, GATEWAY = Gateway)
+
+  Ticket_temp <- fread(t, header = TRUE, sep = ",", stringsAsFactors = FALSE,
+                       integer64 = "numeric")
+
+  Ticket_temp <- Ticket_temp %>%
+    select("ItinID", "RoundTrip", "FarePerMile", "Passengers",
+           "ItinFare", "BulkFare", "Distance") %>%
+    rename(ITIN_ID = ItinID, ROUNDTRIP = RoundTrip,
+           ITIN_YIELD = FarePerMile, PASSENGERS = Passengers,
+           ITIN_FARE = ItinFare, BULKFARE = BulkFare, DISTANCE_FULL = Distance)
 
 
   write.csv(Ticket_temp,
